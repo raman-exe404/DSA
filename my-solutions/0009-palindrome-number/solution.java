a4745189134c1952bc8557acc9d36e991505836f
Ramandeep Singh <ramandeepsinghrds2006@gmail.com>
2025-05-23 13:12:49 +0000

    Sync LeetCode submission - Palindrome Number - Runtime - 4 ms (100.00%), Memory - 44.2 MB (99.98%)

---
+++ my-solutions/0009-palindrome-number/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int n=x;
+        int rev =0;
+        while (n!=0)
+        {
+            int d=n%10;
+            rev=rev*10+d;
+            n=n/10;
+        }
+        if(x<0)
+        {
+            return false;
+        }
+        if(rev==x)
+        {
+            return true;
+        }
+        else
+        return false;
+    }
+}
