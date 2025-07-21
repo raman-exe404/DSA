96a94196cf65b3fb40efbab432b89ccfeca31ea5
Ramandeep Singh <ramandeepsinghrds2006@gmail.com>
2025-07-21 13:54:41 +0000

    Sync LeetCode submission - Find the Index of the First Occurrence in a String - Runtime - 0 ms (100.00%), Memory - 41.9 MB (99.96%)

---
+++ my-solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int l1 = haystack.length();
+        int l2 = needle.length();
+
+        if (l2 == 0) return 0; // Edge case: empty needle
+        if (l1 < l2) return -1;
+
+        for (int i = 0; i <= l1 - l2; i++) {
+            if (haystack.substring(i, i + l2).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
