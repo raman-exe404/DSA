84db3c7333fb9102f2a3f9a951a7d115a82ad26e
Ramandeep Singh <ramandeepsinghrds2006@gmail.com>
2026-01-25 09:21:42 +0000

    Sync LeetCode submission - Transform Array by Parity - Runtime - 4 ms (55.35%), Memory - 47.3 MB (7.30%)

---
+++ my-solutions/3778-transform-array-by-parity/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] transformArray(int[] nums) {
+        for(int i=0;i<=nums.length-1;i++)
+        {
+            if(nums[i]%2==0)
+            {
+                nums[i]=0;
+            }
+            else
+            nums[i]=1;
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
