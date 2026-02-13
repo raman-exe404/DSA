976b5046470f9bc88c975940ccf8a199d6cea337
Ramandeep Singh <ramandeepsinghrds2006@gmail.com>
2026-02-13 18:22:06 +0000

    Sync LeetCode submission - Number of Good Pairs - Runtime - 1 ms (87.47%), Memory - 42.9 MB (43.42%)

---
+++ my-solutions/1635-number-of-good-pairs/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int c=0;
+        for(int i=0;i<=nums.length-1;i++)
+        {
+            for(int j=i+1;j<=nums.length-1;j++)
+            {
+                if(nums[i]==nums[j]&&i<j)
+                {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}
