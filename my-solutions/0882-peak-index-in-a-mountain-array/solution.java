65e0add146e4a4e177f0b6658b15352f0425707d
Ramandeep Singh <ramandeepsinghrds2006@gmail.com>
2026-03-26 11:17:39 +0000

    Sync LeetCode submission - Peak Index in a Mountain Array - Runtime - 0 ms (100.00%), Memory - 80.4 MB (40.28%)

---
+++ my-solutions/0882-peak-index-in-a-mountain-array/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] nums) {
+         int len=nums.length;
+       int st=0,mid,end=len-1;
+       while(st<end)
+       {
+        mid=st+(end-st)/2;
+        if(nums[mid]>nums[mid+1])
+        end=mid;
+        else
+        st=mid+1;
+       } 
+       return st;
+    }
+}
