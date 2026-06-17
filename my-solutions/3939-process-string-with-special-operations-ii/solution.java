fd00730a11d24e65f2f00abe220c2314c358aeec
Ramandeep Singh <ramandeepsinghrds2006@gmail.com>
2026-06-17 12:20:37 +0000

    Sync LeetCode submission - Process String with Special Operations II - Runtime - 54 ms (6.92%), Memory - 47.8 MB (76.90%)

---
+++ my-solutions/3939-process-string-with-special-operations-ii/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public char processStr(String s,long k) {
+        long len=0;
+
+        for(char c:s.toCharArray()) {
+            if(Character.isLowerCase(c)) {
+                len++;
+            } else if(c=='*') {
+                if(len>0) len--;
+            } else if(c=='#') {
+                len*=2;
+            }
+        }
+
+        if(k>=len) return '.';
+
+        for(int i=s.length()-1;i>=0;i--) {
+            char c=s.charAt(i);
+
+            if(Character.isLowerCase(c)) {
+                if(k==len-1) return c;
+                len--;
+            } else if(c=='*') {
+                len++;
+            } else if(c=='#') {
+                len/=2;
+                k%=len;
+            } else if(c=='%') {
+                k=len-1-k;
+            }
+        }
+
+        return '.';
+    }
+}
