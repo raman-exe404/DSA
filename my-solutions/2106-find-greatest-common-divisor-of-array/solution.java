32b09d27043b89f9af9b2cccfd8c94cab6b5a72e
Ramandeep Singh <ramandeepsinghrds2006@gmail.com>
2026-07-18 18:45:14 +0000

    Sync LeetCode submission - Find Greatest Common Divisor of Array - Runtime - 0 ms (100.00%), Memory - 44.8 MB (93.12%)

--- my-solutions/2106-find-greatest-common-divisor-of-array/solution.java
+++ my-solutions/2106-find-greatest-common-divisor-of-array/solution.java
@@ -1,21 +1,25 @@
 class Solution {
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
     public int findGCD(int[] nums) {
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<min){
-                min=nums[i];
-            }
-            if(nums[i]>max){
-                max=nums[i];
-            }
+
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
-        int  ans=1;
-        for(int i=1;i<=min;i++){
-             if(min%i==0 && max%i==0){
-                ans=i;
-             }
-        }
-        return ans;
+
+        return gcd(min, max);
     }
 }
